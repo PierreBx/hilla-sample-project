99bfcaa5339d6c1cc4d9e84c044a2e214812ce84
SwiftBob <pierre_rougier@yahoo.fr>
2024-01-01 21:17:05 +0100

    v0.0.3 [MOD] adding 'Odalisque'

--- frontend/views/MainLayout.tsx
+++ frontend/views/MainLayout.tsx
@@ -15,13 +15,13 @@ export default function MainLayout() {
     <AppLayout primarySection="drawer">
       <div slot="drawer" className="flex flex-col justify-between h-full p-m">
         <header className="flex flex-col gap-m">
-          <h1 className="text-l m-0">My App</h1>
+          <h1 className="text-l m-0">Odalisque</h1>
           <nav>
             <NavLink className={navLinkClasses} to="/">
               Contacts
             </NavLink>
             <NavLink className={navLinkClasses} to="/about">
-              About
+              A propos
             </NavLink>
           </nav>
         </header>
--- frontend/views/about/AboutView.tsx
+++ frontend/views/about/AboutView.tsx
@@ -1,7 +1,7 @@
 export default function AboutView() {
   return (
     <div className="p-m">
-      <h2>Hilla full-stack tutorial app</h2>
+      <h2>Odalisque on SpringBoot</h2>
       <p>
         You can find the steps for building this app in the <a href="https://hilla.dev/docs">Hilla documentation</a>.
       </p>
